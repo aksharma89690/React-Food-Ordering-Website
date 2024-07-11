b08369c4e0ba002c03a41b8d037149f45d0c19de
unknown <akshat>
2024-07-11 18:13:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Section2 from "./pages/Home/Section2";
+import Section3 from "./pages/Home/Section3";
+import Section5 from "./pages/Home/Section5";
+import Section6 from "./pages/Home/Section6";
+import Section7 from "./pages/Home/Section7";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Section2/>} />
+        <Route path="/menu" element={<Section3/>} />
+        <Route path="/shop" element={<Section5/>} />
+        <Route path="/blog" element={<Section6/>} />
+        <Route path="/contact" element={<Section7/>} />
+
+        
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Section7.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Layout from "../../components/Layouts/Layout";
+
+function Section7() {
+  return (
+    <Layout>
+    <section className="contact_section">
+      <Container>
+        <Row className="justify-content-center">
+          <Col sm={8} className="text-center">
+            <h4>We Guarantee</h4>
+            <h2>30 Minutes Delivery!</h2>
+            <p>
+              Aliquam a augue suscipit, luctus neque purus ipsum neque undo
+              dolor primis libero tempus, blandit a cursus varius luctus neque
+              magna
+            </p>
+            <Link to="/" className="btn btn_red px-4 py-2 rounded-0">
+              Call: 999-888-7777
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    </Layout>
+  );
+}
+
+export default Section7;
